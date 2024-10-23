bc58f9712ec532574f1a8df7860779079f43fb14
duonghongduc2k4 <duc2022004@gmail.com>
2024-10-23 09:20:53 +0700

    up

---
+++ src/main/java/com/example/linkup_be/LinkUpBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkup_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkUpBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkUpBeApplication.class, args);
+    }
+
+}
